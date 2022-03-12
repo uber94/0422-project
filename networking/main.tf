e4fa7f8e7d9b3a4553a490870f951b58f84a976b
Hubert NANG <48485474+uber94@users.noreply.github.com>
2022-03-12 14:46:35 +0100

    Update main.tf

--- networking/main.tf
+++ networking/main.tf
@@ -20,7 +20,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public_cidrs[count.index]
   map_public_ip_on_launch = true
-  availability_zone       = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"][count.index]
+  availability_zone       = ["eu-west-3a", "eu-west-3b", "eu-west-3c"][count.index]
 
   tags = {
     Name = "public_${count.index + 1}"
